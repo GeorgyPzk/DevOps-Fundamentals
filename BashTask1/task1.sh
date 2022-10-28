89de62f223aa323fd49054df19bd01893fb4fb90
georgy <georgyVSwork@yandex.ru>
2022-10-28 16:40:42 +0300

    fix bash task1

--- BashTask1/task1.sh
+++ BashTask1/task1.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 csv_file_path=$1
-echo $csv_file_path
+#echo $csv_file_path
 #/home/georgy/task1
 part_csv_file_path=${csv_file_path::${#csv_file_path}-4} # delite last 4 symbols
 new_csv_file_path="${part_csv_file_path}_new.csv"
 rm -R $new_csv_file_path
 touch $new_csv_file_path
+
 while IFS=, read -r field1 field2 field3 field4 field5 field6 # read all line and separate columns
 do
 if [[ "$field1" = "id" ]]; then
@@ -15,9 +16,10 @@ else
     read -a name <<< "$field3"  # separating name 
     field3="${name[0]^} ${name[1]^}" # Write name with upper frist symbol
     name[1]=$(echo ${name[1]} | sed 's/-//g') # Replace -
-
-    field5="${name[0]:0:1}${name[1]}${field2}@abc.com" # Compiling an email
-    field5="${field5,,}" # lower case
+    #field5="${name[0]:0:1}${name[1]}${field2}@abc.com" # Compiling an email
+    email5="${name[0]:0:1}${name[1]}${field2}@abc.com" # Compiling an email
+    email5=$(echo ${email5} | sed 's/-//g') # Replace -
+    field5="${email5,,}" # lower case
     echo "${field1},${field2},${field3},${field4},${field5},${field6}" >> $new_csv_file_path # write to file
 fi
 done < $csv_file_path 
