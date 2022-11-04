23719f307dc6e9a3c01d83381736560f3a17c077
georgy <georgyVSwork@yandex.ru>
2022-11-04 08:40:37 +0300

    fix email bash 1 task

--- BashTask1/task1.sh
+++ BashTask1/task1.sh
@@ -7,6 +7,14 @@ new_csv_file_path="${part_csv_file_path}_new.csv"
 rm -R $new_csv_file_path
 touch $new_csv_file_path
 
+oldfield1="0"
+oldfield2="0"
+oldfield3="0"
+oldfield4="0"
+oldfield5="0"
+oldfield6="0"
+
+
 while IFS=, read -r field1 field2 field3 field4 field5 field6 field7 field8 # read all line and separate columns
 do
 if [[ "$field1" = "id" ]]; then
@@ -26,11 +34,30 @@ else
     IFS=" " # separator sign for name
     read -a name <<< "$field3"  # separating name
     field3="${name[0]^} ${name[1]^}" # Write name with upper frist symbol
-    name[1]=$(echo ${name[1]} | sed 's/-//g') # Replace -
-    #field5="${name[0]:0:1}${name[1]}${field2}@abc.com" # Compiling an email
-    email5="${name[0]:0:1}${name[1]}${field2}@abc.com" # Compiling an email
-    email5=$(echo ${email5} | sed 's/-//g') # Replace -
-    field5="${email5,,}" # lower case
-    echo "${field1},${field2},${field3},${field4},${field5},${field6}" >> $new_csv_file_path # write to file
+
+    if [[ "$oldfield3" == "$field3" ]]; then 
+        name[1]=$(echo ${name[1]} | sed 's/-//g') # Replace -
+        email5="${name[0]:0:1}${name[1]}${field2}@abc.com" # Compiling an email
+        field5="${email5,,}" # lower case
+        #fix old email
+        oldemail5="${name[0]:0:1}${name[1]}${oldfield2}@abc.com" # Compiling an email
+        oldfield5="${oldemail5,,}" # lower case
+    else 
+        name[1]=$(echo ${name[1]} | sed 's/-//g') # Replace -
+        email5="${name[0]:0:1}${name[1]}@abc.com" # Compiling an email
+        field5="${email5,,}" # lower case
+    fi
+
+    if [[ "$oldfield1" != "0" ]]; then 
+        echo "${oldfield1},${oldfield2},${oldfield3},${oldfield4},${oldfield5},${oldfield6}" >> $new_csv_file_path # write
+    fi
+    oldfield1=$field1
+    oldfield2=$field2
+    oldfield3=$field3
+    oldfield4=$field4
+    oldfield5=$field5
+    oldfield6=$field6
 fi
 done < $csv_file_path 
+
+echo "${oldfield1},${oldfield2},${oldfield3},${oldfield4},${oldfield5},${oldfield6}" >> $new_csv_file_path
