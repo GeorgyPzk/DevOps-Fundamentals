ffe5e1d99016b6be76aed1b5b70850ecf2be4725
georgy <georgyVSwork@yandex.ru>
2022-11-04 18:55:50 +0300

    fix email bash1 ps2

--- PSTask2/task2.ps1
+++ PSTask2/task2.ps1
@@ -18,25 +18,27 @@ begin {
 process {
     try {
         Write-Host "Processing..." -ForegroundColor DarkGray
-
         $accounts = Import-Csv $csv_file_path
-        
-        foreach ($item in $accounts) {
-            # Name format all letters to lower, title latters to upper
-            $item.name = ( Get-Culture ).TextInfo.ToTitleCase( $item.name.ToLower() )
-            # email update
-            $nameArr = $item.name.Split(" ") 
-            #$item.email = "$($namearr[0][0])$($namearr[1])$($item.location_id)@abc.com".ToLower() -replace '\W', ''
-            $partemail =  "$($namearr[0][0])$($namearr[1])$($item.location_id)".ToLower() -replace '\W', ''
-            $email = $partemail + '@abc.com'
-            #Write-Host "$email" -ForegroundColor Gree
-            $item.email = $email
-        }
 
+        foreach ($item in $accounts) {
+            if (($accounts | ? { $_.Name -eq $item.name }).Count -ge 1){
+                # Name format all letters to lower, title latters to upper
+                $item.name = ( Get-Culture ).TextInfo.ToTitleCase( $item.name.ToLower() )
+                # email update
+                $nameArr = $item.name.Split(" ")
+                $item.email = ("$($nameArr[0][0])$($nameArr[1])$($item.location_id)".ToLower() -replace '\W', '') + "@abc.com"
+            } else {
+                # Name format all letters to lower, title latters to upper
+                $item.name = ( Get-Culture ).TextInfo.ToTitleCase( $item.name.ToLower() )
+                # email update
+                $nameArr = $item.name.Split(" ")
+                $item.email = ("$($nameArr[0][0])$($nameArr[1])".ToLower() -replace '\W', '') + "@abc.com"
+            }
+        }
+        
         # Generate new file path and export csv object
         $newPath = $csv_file_path.Insert($csv_file_path.Length-4,"_new")
         $accounts | Export-Csv -Path $newPath -NoTypeInformation
-        #Write-Host "Path of new file: $newPath" -ForegroundColor Green
 
     } catch {
         $isErrorState = $true
